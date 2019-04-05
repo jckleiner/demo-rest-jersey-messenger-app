7bbbc92bf485fa2b727e5c02b9eca95dad117fec
Jonathan-Can Kleiner <jkleiner@uni-potsdam.de>
2019-04-05 18:27:48 +0200

    small change to test jenkins job4

--- src/main/java/com/greydev/messenger_demo/MyResource.java
+++ src/main/java/com/greydev/messenger_demo/MyResource.java
@@ -20,6 +20,6 @@ public class MyResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
-        return "aasdasd";
+        return "VAPE NASH YALL -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-**---****-";
     }
 }
