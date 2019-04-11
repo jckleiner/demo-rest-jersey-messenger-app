a74fad1ca29ce9a0a7f73ea86ebcd5da49e61d7a
Jonathan-Can Kleiner <jkleiner@uni-potsdam.de>
2019-04-11 16:09:11 +0200

    update gitignore, remove target folder

--- src/main/java/com/greydev/messenger_demo/MyResource.java
+++ src/main/java/com/greydev/messenger_demo/MyResource.java
@@ -20,6 +20,6 @@ public class MyResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
-        return "deleted some stuff";
+        return "deleted some stuff asdasdasd";
     }
 }
