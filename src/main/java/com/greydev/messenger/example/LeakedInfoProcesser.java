cbecfaedeede126cdf673853b0c2b5d79706a1fe
Jonathan-Can Kleiner <jkleiner@uni-potsdam.de>
2019-05-03 13:17:02 +0200

    test

--- src/main/java/com/greydev/messenger/example/LeakedInfoProcesser.java
+++ src/main/java/com/greydev/messenger/example/LeakedInfoProcesser.java
@@ -1,29 +1,39 @@
 package com.greydev.messenger.example;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 
-@Path("/")
+@Path("/stealingdata")
 public class LeakedInfoProcesser {
-
-	private static final List<String> dataList = new ArrayList<>();
 
 	@GET
 	public String getData(@QueryParam("data") String userData) {
-		String result = "Saved user data: ";
-		if (userData != null) {
-			dataList.add(userData);
-		}
 
-		for (String s : dataList) {
-			result += s + " ";
-		}
+		// save user data to database
 
-		return result;
+		return "Saved user data: " + userData;
 	}
 
 }
+
+//@Path("/")
+//public class LeakedInfoProcesser {
+//
+//	private static final List<String> dataList = new ArrayList<>();
+//
+//	@GET
+//	public String getData(@QueryParam("data") String userData) {
+//		String result = "Saved user data: ";
+//		if (userData != null) {
+//			dataList.add(userData);
+//		}
+//
+//		for (String s : dataList) {
+//			result += s + " ";
+//		}
+//
+//		return result;
+//	}
+//
+//}
